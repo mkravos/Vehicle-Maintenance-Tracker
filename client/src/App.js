363896119f752106965e521aa1ef8501d9e6dfe1
mkravos <mikekra@outlook.com>
2022-05-18 22:44:17 -0500

    Updated site colors

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,14 @@ import { Button, Form } from 'react-bootstrap';
 function App() {
   return (
     <Router>
-      <div>
+      <body>
         <Routes>
           <Route exact path="/" element={<Login/>}></Route>
           <Route exact path="/dashboard" element={<Dashboard/>}></Route>
           <Route exact path="/garage" element={<Garage/>}></Route>
           <Route exact path="/report" element={<Report/>}></Route>
         </Routes>
-      </div>
+      </body>
     </Router>
   );
 }
