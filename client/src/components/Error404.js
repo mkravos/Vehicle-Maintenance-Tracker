4ca963f6bd5a084ad0d0022114f72ac5a9599065
MajASAR <majkrav@asardigital.com>
2022-05-31 16:35:30 -0500

    Added home button to 404 page.

--- client/src/components/Error404.js
+++ client/src/components/Error404.js
@@ -1,3 +1,6 @@
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 function Error404() {
     return (
         <div className="e404">
@@ -6,6 +9,7 @@ function Error404() {
             <div>
               <p>404: There is nothing at this address.</p>
               <a href="https://www.freepik.com/vectors/modern-texture" target="_blank" rel="noopener noreferrer" className="Background-attribution">Modern texture vector created by rawpixel.com - www.freepik.com</a>
+              <center><Link to="/dashboard"><Button variant="outline-primary">Go Home</Button></Link></center>
             </div>
           </header>
         </div>
