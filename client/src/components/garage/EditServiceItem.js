46f201670786a31d643d33988b40f02ad8fda0fb
MajASAR <majkrav@asardigital.com>
2022-08-12 15:02:33 -0500

    add/edit service record modal styling

--- client/src/components/garage/EditServiceItem.js
+++ client/src/components/garage/EditServiceItem.js
@@ -19,24 +19,16 @@ function EditServiceItem({id}) {
           <Modal.Body>
             <Form className="form-control-lg">
               <Form.Group className="mb-3" controlId="service-name-box">
-                <Form.Label>Name</Form.Label>
+                <Form.Label>Name *</Form.Label>
                 <Form.Control className="servicenamebox" placeholder="Enter part name" />
               </Form.Group>
               <Form.Group className="mb-3" controlId="service-date-box">
-                <Form.Label>Date</Form.Label>
+                <Form.Label>Date *</Form.Label>
                 <Form.Control placeholder="Enter date of service" />
               </Form.Group>
               <Form.Group className="mb-3" controlId="service-mileage-box">
-                <Form.Label>Mileage</Form.Label>
+                <Form.Label>Mileage *</Form.Label>
                 <Form.Control placeholder="Enter mileage at time of service" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="mileage-interval-box">
-                <Form.Label>Service Interval (Miles)</Form.Label>
-                <Form.Control placeholder="Enter service interval (miles)" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="time-interval-box">
-                <Form.Label>Next Service Interval (Date)</Form.Label>
-                <Form.Control placeholder="Enter next service interval (date)" />
               </Form.Group>
               <Form.Group className="mb-3" controlId="part-number-box">
                 <Form.Label>Part Number</Form.Label>
@@ -46,12 +38,16 @@ function EditServiceItem({id}) {
                 <Form.Label>Cost</Form.Label>
                 <Form.Control placeholder="Enter cost of service" />
               </Form.Group>
-              <Form.Group className="mb-3" controlId="attach-receipt-box">
-                <Form.Label>Attach Part Receipt:</Form.Label>
-                <div className="RSI-attach-file">
-                  <Button variant="outline-primary" className="Attach-btn">Attach</Button>
-                  <div id="filename" className="Modal-filename text-muted">(no file attached)</div>
-                </div>
+              <Form.Group className="mb-3" controlId="mileage-interval-box">
+                <Form.Label>Service Interval</Form.Label>
+                <Form.Control placeholder="Enter service interval (miles)" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="time-interval-box">
+                <Form.Control placeholder="Enter service interval (date)" />
+              </Form.Group>
+              <Form.Group className="mb-3 RSI-attach-file" controlId="attach-receipt-box">
+                <Button variant="outline-primary" className="Attach-btn">Attach Receipt</Button>
+                <div id="filename" className="Modal-filename text-muted">(no file attached)</div>
               </Form.Group>
             </Form>
           </Modal.Body>
