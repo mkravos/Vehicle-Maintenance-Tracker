0bfac45962dc6452f3cfd8f0c076b9713785d934
mkravos <mikekra@outlook.com>
2023-12-08 18:23:45 -0600

    feature cleanup

--- client/src/components/garage/Garage.js
+++ client/src/components/garage/Garage.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import BootstrapNavbar from '../BootstrapNavbar.js';
 import UpdateMileage from './UpdateMileage.js';
 import RecordServiceItem from './RecordServiceItem.js';
-import GenerateReport from './GenerateReport.js';
 import AddVehicle from './AddVehicle.js';
 import EditVehicle from './EditVehicle.js';
 import RemoveVehicle from './RemoveVehicle.js';
@@ -46,14 +45,14 @@ function Garage() {
     }
   }
 
-  const [ userId, setUserId ] = useState();
-  if(!userId) {
+  const [userId, setUserId] = useState();
+  if (!userId) {
     getUserId()
-    .then(value => {
-      setUserId(value.id);
-    });
+      .then(value => {
+        setUserId(value.id);
+      });
   }
-  
+
   const getVehicles = async (uuid) => {
     try {
       const res = await fetch("http://localhost:1234/get-vehicle-list/" + uuid, {
@@ -67,48 +66,48 @@ function Garage() {
     }
   }
 
-  const [ vehicles, setVehicles ] = useState();
+  const [vehicles, setVehicles] = useState();
   function getVehicleList(id) {
     getVehicles(id)
-    .then(value => {
-      setVehicles(value);
-    });
+      .then(value => {
+        setVehicles(value);
+      });
   }
-  if(userId && !vehicles) {
+  if (userId && !vehicles) {
     getVehicleList(userId);
   }
 
   useEffect(() => {
-    if(userId) {
+    if (userId) {
       setVehicleAdded(false);
       getVehicleList(userId);
     }
-  },[vehicleAdded, userId]);
+  }, [vehicleAdded, userId]);
   useEffect(() => {
-    if(userId) {
+    if (userId) {
       setVehicleEdited(false);
       getVehicleList(userId);
     }
-  },[vehicleEdited, userId]);
+  }, [vehicleEdited, userId]);
   useEffect(() => {
-    if(userId) {
+    if (userId) {
       setVehicleRemoved(false);
       getVehicleList(userId);
     }
-  },[vehicleRemoved, userId]);
+  }, [vehicleRemoved, userId]);
   useEffect(() => {
-    if(userId) {
+    if (userId) {
       setMileageUpdated(false);
       getVehicleList(userId);
     }
-  },[mileageUpdated, userId]);
+  }, [mileageUpdated, userId]);
   useEffect(() => {
     setItemRecorded(false);
-  },[itemRecorded]);
-  
+  }, [itemRecorded]);
+
   return (
     <div className="Garage">
-      <BootstrapNavbar/>
+      <BootstrapNavbar />
       <header className="Garage-header">
         <p className="Page-title">Garage</p>
         {vehicles ? vehicles.map((val, key) => {
@@ -118,33 +117,33 @@ function Garage() {
                 <Card.Title>{val.model_year} {val.make} {val.model}</Card.Title>
                 <div className="Card-text">
                   <div><span className="vehicleItem">Name: </span>{val.vehicle_name}</div>
-                    <div><span className="vehicleItem">Mileage: </span> {val.mileage}</div>
-                    {
-                      val.vin ? <div><span className="vehicleItem">VIN: </span> {val.vin}</div> : null
-                    }
+                  <div><span className="vehicleItem">Mileage: </span> {val.mileage}</div>
+                  {
+                    val.vin ? <div><span className="vehicleItem">VIN: </span> {val.vin}</div> : null
+                  }
                 </div>
                 <div className="Garage-dropdown-container">
                   <DropdownButton className="Actions-dropdown" variant="primary" id="dropdown-basic-button" title="Actions">
-                    <UpdateMileage id={val.id} updatedMileage={updatedMileage}/>
-                    <RecordServiceItem id={val.id} vehicleName={val.vehicle_name} recordedItem={recordedItem}/>
-                    <GenerateReport id={val.id}/>
+                    <UpdateMileage id={val.id} updatedMileage={updatedMileage} />
+                    <RecordServiceItem id={val.id} vehicleName={val.vehicle_name} recordedItem={recordedItem} />
+                    {/* <GenerateReport id={val.id} /> */}
                   </DropdownButton>
                   <DropdownButton className="Vehicle-dropdown" variant="outline-primary" id="dropdown-basic-button" title="Vehicle">
-                    <EditVehicle id={val.id} editedVehicle={editedVehicle}/>
-                    <RemoveVehicle id={val.id} removedVehicle={removedVehicle}/>
+                    <EditVehicle id={val.id} editedVehicle={editedVehicle} />
+                    <RemoveVehicle id={val.id} removedVehicle={removedVehicle} />
                   </DropdownButton>
                 </div>
-                <ServiceRecords 
-                  vehicleId={val.id} 
-                  currMiles={val.mileage} 
-                  vehicleName={val.vehicle_name} 
+                <ServiceRecords
+                  vehicleId={val.id}
+                  currMiles={val.mileage}
+                  vehicleName={val.vehicle_name}
                   itemRecorded={itemRecorded}
                 />
               </Card.Body>
             </Card>
           )
         }) : null}
-        <AddVehicle setNewVehicle={setNewVehicle}/>
+        <AddVehicle setNewVehicle={setNewVehicle} />
       </header>
     </div>
   );
